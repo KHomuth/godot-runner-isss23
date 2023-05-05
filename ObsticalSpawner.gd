e4f98386b904eaba8799d8d2bfb95d9af53eaf54
Jacek Bannach <jacek.bannach@haw-hamburg.de>
2023-05-05 16:19:30 +0200

    add starting position

--- ObsticalSpawner.gd
+++ ObsticalSpawner.gd
@@ -11,6 +11,7 @@ func _createObsitical():
 	sprite2d.texture = texture
 
 	var colisionShape2d = CollisionShape2D.new()
+	colisionShape2d.debug_color = Color(33,33,33,50)
 	var shape = CapsuleShape2D.new()
 	
 	shape.height = 30
@@ -18,14 +19,15 @@ func _createObsitical():
 	
 	colisionShape2d.shape = shape
 	
-	rigidBody2d.add_child(sprite2d)
 	rigidBody2d.add_child(colisionShape2d)
+	rigidBody2d.add_child(sprite2d)
 	
+	rigidBody2d.position = Vector2(500, 0)
 	rigidBody2d.gravity_scale = 0
 	rigidBody2d.linear_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
 	rigidBody2d.linear_damp = 0
 	rigidBody2d.lock_rotation = true
-	
+	rigidBody2d
 	return rigidBody2d
 
 # Called when the node enters the scene tree for the first time.
@@ -40,7 +42,7 @@ func _process(delta):
 	if(timer.is_stopped()):
 		
 		obsticles.push_front(_createObsitical())
-		obsticles.front().linear_velocity = Vector2(10000 * delta,0)  
+		obsticles.front().linear_velocity = Vector2(-10000 * delta,0)  
 		add_child(obsticles.front())
 		timer.start(3)
 		print(obsticles.size())
