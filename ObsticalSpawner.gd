aafa3edf2b93d90f67e2bf7bb1e1ce35fc2ec1c0
Jacek Bannach <jacek.bannach@haw-hamburg.de>
2023-05-07 18:59:13 +0200

    some changes

--- ObsticalSpawner.gd
+++ ObsticalSpawner.gd
@@ -3,7 +3,11 @@ extends Node
 var texture = preload("res://icon.svg")
 var obsticles = []
 var timer = Timer.new()
+var speed = 5
 
+func _onEnterArea(body):
+	print(body)
+	
 func _createObsitical():
 	var rigidBody2d = RigidBody2D.new()
 	
@@ -12,21 +16,22 @@ func _createObsitical():
 
 	var colisionShape2d = CollisionShape2D.new()
 	colisionShape2d.debug_color = Color(33,33,33,50)
-	var shape = CapsuleShape2D.new()
-	
-	shape.height = 30
-	shape.radius = 10
+	var shape = RectangleShape2D.new()
+	shape.size = Vector2(100,100)
 	
 	colisionShape2d.shape = shape
-	
-	rigidBody2d.add_child(colisionShape2d)
+
 	rigidBody2d.add_child(sprite2d)
+	rigidBody2d.add_child(colisionShape2d)
 	
 	rigidBody2d.position = Vector2(500, 0)
 	rigidBody2d.gravity_scale = 0
 	rigidBody2d.linear_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
 	rigidBody2d.linear_damp = 0
 	rigidBody2d.lock_rotation = true
+	
+	rigidBody2d.add_to_group("obstical")
+	
 	return rigidBody2d
 
 # Called when the node enters the scene tree for the first time.
@@ -37,14 +42,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	if(timer.is_stopped()):
 		obsticles.push_front(_createObsitical())
-		obsticles.front().linear_velocity = Vector2(-10000 * delta,0)  
+		obsticles.front().linear_velocity = Vector2(-10000 * speed * delta,0)  
 		add_child(obsticles.front())
-		timer.start(3)
+		timer.start(randi_range(10,12))
 		print(obsticles.size())
-		
+	
 	if(obsticles.size() > 4):
 		obsticles.back().queue_free()
 		obsticles.pop_back()
