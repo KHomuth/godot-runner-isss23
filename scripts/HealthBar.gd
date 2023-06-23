5ce48cffc003e5c04d4dedf42c0a20c4e2f1735a
Jacek Bannach <jacek.bannach@haw-hamburg.de>
2023-06-23 21:28:47 +0200

    add hit sound

--- scripts/HealthBar.gd
+++ scripts/HealthBar.gd
@@ -1,9 +1,11 @@
 extends TextureProgressBar
 
+@onready var _audio_player = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	value = 100
 
 func _on_character_body_2d_hit():
+	_audio_player.play(0)
 	value -= 50
